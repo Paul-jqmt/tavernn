55ea1fbd79cf9312fd45b8e28781dcc1739c0813
pauljacquemot <jacque_p@etna-alternance.net>
2025-04-25 18:05:54 +0200

    CreateClub mais il faut fix les api de création de club, landing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Clubs from './pages/Clubs'
+import Landing from './pages/Landing'
+import CreateClub from "./pages/CreateClub"
 
 function App() {
     return (
         <Routes>
             <Route path="/clubs" element={<Clubs />} />
+            <Route path="/" element={<Landing />} />
+            <Route path="/clubs/create" element={<CreateClub />} />
         </Routes>
     )
 }
