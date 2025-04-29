c39b46ebdf45f3548fba0f6355abc5c65e0a8e0c
pauljacquemot <jacque_p@etna-alternance.net>
2025-04-30 00:33:11 +0200

    new database

--- backend/src/main/resources/db/migration/V2__CREATE_TABLE.sql
+++ backend/src/main/resources/db/migration/V2__CREATE_TABLE.sql
@@ -1,76 +1,157 @@
 USE `tavernn`;
 
-CREATE TABLE IF NOT EXISTS users
-(
-    id                VARCHAR(36) PRIMARY KEY,
-    email             VARCHAR(255) NOT NULL,
-    password          VARCHAR(255) NOT NULL,
-    username          VARCHAR(100) NOT NULL,
-    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    role              VARCHAR(50),
-    discord           VARCHAR(100),
-    level             VARCHAR(50),
-    available_time    VARCHAR(255),
-    experience        VARCHAR(255),
-    looking_for_team  BOOLEAN   DEFAULT FALSE
-);
-
 CREATE TABLE IF NOT EXISTS games
 (
+    id   VARCHAR(36) PRIMARY KEY,
+    name VARCHAR(36) NOT NULL UNIQUE,
+    image VARCHAR(255),
+    team_max_nr INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    id VARCHAR(36) PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    registration_date DATE NOT NULL DEFAULT (CURRENT_DATE),
+    discord VARCHAR(100),
+    logo VARCHAR(255),
+    open_at_invite BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+CREATE TABLE IF NOT EXISTS user_games (
+    user_id    VARCHAR(36) NOT NULL,
+    game_id    VARCHAR(36) NOT NULL,
+    game_level ENUM('beginner', 'intermediate', 'advanced', 'professional'),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (game_id) REFERENCES games(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS clubs (
+    id               VARCHAR(36) PRIMARY KEY,
+    name             VARCHAR(255) NOT NULL UNIQUE,
+    description      TEXT,
+    creation_date    DATE NOT NULL DEFAULT (CURRENT_DATE),
+    logo             VARCHAR(255),
+    club_type ENUM('open', 'closed', 'invite_only') NOT NULL DEFAULT 'open',
+    nr_members INT NOT NULL DEFAULT 0,
+    max_members INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS club_members (
+    club_id VARCHAR(36) NOT NULL,
+    user_id VARCHAR(36) NOT NULL,
+    is_owner BOOLEAN NOT NULL DEFAULT FALSE,
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE,
+    FOREIGN KEY (club_id) REFERENCES clubs(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS teams (
     id          VARCHAR(36) PRIMARY KEY,
     name        VARCHAR(100) NOT NULL,
     description TEXT,
-    image       VARCHAR(255),
-    category    VARCHAR(50)
+    club_id     VARCHAR(36) NOT NULL,
+    game_id     VARCHAR(36) NOT NULL,
+    nr_members  INT NOT NULL DEFAULT 0,
+    FOREIGN KEY (club_id) REFERENCES clubs(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (game_id) REFERENCES games(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS user_games
-(
-    id         VARCHAR(36) PRIMARY KEY,
-    user_id    VARCHAR(36),
-    game_id    VARCHAR(36),
-    game_level VARCHAR(50),
-    FOREIGN KEY (user_id) REFERENCES users (id),
-    FOREIGN KEY (game_id) REFERENCES games (id)
+CREATE TABLE IF NOT EXISTS team_members (
+    team_id    VARCHAR(36) NOT NULL,
+    user_id    VARCHAR(36) NOT NULL,
+    is_captain BOOLEAN NOT NULL DEFAULT FALSE,
+    FOREIGN KEY (team_id) REFERENCES teams(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS clubs
-(
-    id               VARCHAR(36) PRIMARY KEY,
-    name             VARCHAR(100) NOT NULL,
-    description      TEXT,
-    creation_date    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    logo             VARCHAR(255),
-    open_recruitment BOOLEAN   DEFAULT TRUE,
-    level            VARCHAR(50)
+CREATE TABLE IF NOT EXISTS club_join_requests (
+    id VARCHAR(36) PRIMARY KEY,
+    message TEXT,
+    user_id VARCHAR(36) NOT NULL,
+    club_id VARCHAR(36) NOT NULL,
+    date DATE NOT NULL DEFAULT (CURRENT_DATE),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (club_id) REFERENCES clubs(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS teams
-(
-    id          VARCHAR(36) PRIMARY KEY,
-    club_id     VARCHAR(36),
-    name        VARCHAR(100) NOT NULL,
-    game_id     VARCHAR(36),
-    level       VARCHAR(50),
-    max_players INTEGER,
-    objectives  TEXT,
-    FOREIGN KEY (club_id) REFERENCES clubs (id),
-    FOREIGN KEY (game_id) REFERENCES games (id)
+CREATE TABLE IF NOT EXISTS team_join_requests (
+    id VARCHAR(36) PRIMARY KEY,
+    message TEXT,
+    user_id VARCHAR(36) NOT NULL,
+    team_id VARCHAR(36) NOT NULL,
+    date DATE NOT NULL DEFAULT (CURRENT_DATE),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE,
+    FOREIGN KEY (team_id) REFERENCES teams(id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS team_users
-(
-    id         VARCHAR(36) PRIMARY KEY,
-    team_id    VARCHAR(36),
-    user_id    VARCHAR(36),
-    role       VARCHAR(50),
-    is_captain BOOLEAN DEFAULT FALSE,
-    FOREIGN KEY (team_id) REFERENCES teams (id),
-    FOREIGN KEY (user_id) REFERENCES users (id)
+CREATE TABLE IF NOT EXISTS user_notifications (
+    id VARCHAR(36) PRIMARY KEY,
+    user_id VARCHAR(36) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    message TEXT NOT NULL,
+    date DATE NOT NULL DEFAULT (CURRENT_DATE),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS trainings
-(
+CREATE TABLE IF NOT EXISTS club_post (
+    id VARCHAR(36) PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    user_id VARCHAR(36) NOT NULL,
+    club_id VARCHAR(36) NOT NULL,
+    message TEXT NOT NULL,
+    private BOOLEAN NOT NULL DEFAULT TRUE,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE,
+    FOREIGN KEY (club_id) REFERENCES clubs(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS team_post (
+    id VARCHAR(36) PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    user_id VARCHAR(36) NOT NULL,
+    team_id VARCHAR(36) NOT NULL,
+    message TEXT NOT NULL,
+    private BOOLEAN NOT NULL DEFAULT TRUE,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE,
+    FOREIGN KEY (team_id) REFERENCES teams(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS trainings (
     id         VARCHAR(36) PRIMARY KEY,
     team_id    VARCHAR(36),
     start_date TIMESTAMP,
