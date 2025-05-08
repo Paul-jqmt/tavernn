8dd1dda90ba0eb8a90fa9fbaa9820b5321f942fa
Lucas <lucasj35@hotmail.fr>
2025-05-08 10:09:28 +0200

    improve login controller

---
+++ backend/src/main/java/etna/tavernn/auth/service/AuthService.java
@@ -0,0 +1,42 @@
+package etna.tavernn.auth.service;
+
+import etna.tavernn.auth.dto.AuthResponse;
+import etna.tavernn.user.model.User;
+import etna.tavernn.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+
+    public AuthResponse authenticateAndCreateToken(String email, String password) {
+
+        Optional<User> userOptional = userRepository.findByEmail(email);
+        if (userOptional.isEmpty()) {
+            return null;
+        }
+
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(email, password)
+            );
+
+            User user = userOptional.get();
+            UserDetails userDetails = jwtService.createUserDetails(user);
+            return jwtService.createTokenResponse(userDetails, user);
+
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
