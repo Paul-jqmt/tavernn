9492423d6665c46981f00938a0b65072cd12cbc4
Lucas <lucasj35@hotmail.fr>
2025-05-08 19:22:18 +0200

    fix imports

--- backend/src/main/java/etna/tavernn/auth/dto/ErrorResponse.java
+++ backend/src/main/java/etna/tavernn/auth/dto/ErrorResponse.java
@@ -1,6 +1,9 @@
 package etna.tavernn.auth.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
