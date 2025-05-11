5a23d8c82ad8a32f244c9da10f8b2ab94c4551c7
Lucas <lucasj35@hotmail.fr>
2025-05-11 12:59:36 +0200

    clean code

--- frontend/src/components/common/ui/form.tsx
+++ frontend/src/components/common/ui/form.tsx
@@ -2,13 +2,13 @@ import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
 import {Slot} from "@radix-ui/react-slot"
 import {
-  Controller,
-  type ControllerProps,
-  type FieldPath,
-  type FieldValues,
-  FormProvider,
-  useFormContext,
-  useFormState,
+    Controller,
+    type ControllerProps,
+    type FieldPath,
+    type FieldValues,
+    FormProvider,
+    useFormContext,
+    useFormState,
 } from "react-hook-form"
 
 import {cn} from "@/lib/utils.ts"
